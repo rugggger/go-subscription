ee19d191a1cf502ae582910b0c3cdd91691dddc8
rugggger <yaron.bar@contentsquare.com>
2022-07-29 10:45:26 +0300

    routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,7 +11,14 @@ func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.SessionLoad)
 
 	mux.Get("/", app.HomePage)
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Post("/activate-account", app.ActivateAcount)
 	return mux
 }
